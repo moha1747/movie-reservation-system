017d433b9912badeed89bc288074a330d4a4e733
moha1747 <mohamud.abbaas14@gmail.com>
2025-05-23 17:27:25 -0500

    Fix folder structure

--- repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.movie_res_system.repository;
-
-import com.backend.movie_res_system.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
